2aa8c758f875b84a292b51edc4e6e6902c8a1473
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-07 22:31:04 +0100

    feat: Minor Change

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,41 +5,43 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-		fontFamily: {
-			sans: ['Manrope', 'sans-serif'],
-		},
-		fontSize: {
-			xs: '.815rem',
-			sm: '.875rem',
-			base: '.94rem',
-			lg: '1.125rem',
-			'2xl': '1.5rem',
-			'3xl': '1.75rem',
-			'4xl': '2rem',
-			'5xl': '2.5rem',
-			'6xl': '3.5rem',
-		},
-		screens: {
-			md: '768px',
-			lg: '1110px',
-		},
-		colors: {
-			orange: {
-				'primary': '#FBAF85',
-				600: '#D87D4A',
-			},
-			white: {
-                'text': '#FFFFFF',
-				'primary': '#FAFAFA',
-				'secondary': '#F1F1F1',
-			},
-			black: '#191919',
-		},
-		width: {
-			'3/10': '30%',
-			'7/10': '70%',
-			'9/10': '90%',
-		},
-	},
+    extend: {
+		  fontFamily: {
+			  sans: ['Manrope', 'sans-serif'],
+		  },
+		  fontSize: {
+			  xs: '.815rem',
+			  sm: '.875rem',
+			  base: '.94rem',
+			  lg: '1.125rem',
+			  '2xl': '1.5rem',
+			  '3xl': '1.75rem',
+			  '4xl': '2rem',
+			  '5xl': '2.5rem',
+			  '6xl': '3.5rem',
+		  },
+		  screens: {
+			  md: '768px',
+			  lg: '1110px',
+		  },
+		  colors: {
+			  orange: {
+				  'primary': '#FBAF85',
+				  600: '#D87D4A',
+			  },
+			  white: {
+				  'text': '#FFFFFF',
+				  'primary': '#FAFAFA',
+				  'secondary': '#F1F1F1',
+			  },
+			  black: '#191919',
+		  },
+		  width: {
+			  '3/10': '30%',
+			  '7/10': '70%',
+			  '9/10': '90%',
+		  },
+    },
+  },
   plugins: [],
 }
