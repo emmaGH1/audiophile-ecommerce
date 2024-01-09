4f869a9e6c4bda9bb132134338fb32fb54dc9ab9
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-09 23:19:12 +0100

    feat: Add navlinks

---
+++ app/lib/constants.ts
@@ -0,0 +1,24 @@
+interface NavLinks {
+  label: string,
+  path: string,
+  imgUrl?: string
+}[]
+
+export const NavLinks = [
+            {
+            label: 'Home',
+            path: '/',
+            },
+            {
+            label: 'Headphones',
+            path: '/headphones',
+            },
+            {
+                label: 'Speakers',
+                path: '/speakers'
+            },
+            {
+                label: 'Earphones',
+                path: '/earphones'
+            },
+]
