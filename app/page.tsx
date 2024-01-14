1e7cf0874318311d69232ab4c3a4baf525ad8740
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-14 23:50:46 +0100

    feat: Remove hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Hero from "@/components/ui/home/Hero";
+
 import { fetchData } from "./lib/data";
 
 const Page = () => {
 
   return (
-    <div className="">
-      <Hero />
+    <div className="flex flex-col">
     </div>
   );
 };
