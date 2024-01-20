df02da240f4fac13fc18ad4902027c2998d94df9
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-20 23:35:26 +0100

    feat: Update button component

--- components/ui/shared/Button.tsx
+++ components/ui/shared/Button.tsx
@@ -2,14 +2,15 @@ import { cn } from "@/utils/cn";
 import Link from "next/link";
 
 interface ButtonProps {
-   classNames?: string,
-   children?: string 
+   className?: string,
+   children?: string,
+   linkTo?: string
 }
 
-export const Button = ({ classNames, children = 'see product' }: ButtonProps) => {
+export const Button = ({ className, children = 'see product', linkTo = '/' }: ButtonProps) => {
   return (
-    <Link href='/'>
-        <button type="button" className={cn("px-8 py-3 uppercase font-bold text-sm", `bg-orange-500 text-white-text ${classNames} `)}>
+    <Link href={linkTo}>
+        <button type="button" className={cn("px-8 py-3 uppercase font-bold text-sm", `bg-orange-500 text-white-text ${className} `)}>
           {children}
         </button>
     </Link>
