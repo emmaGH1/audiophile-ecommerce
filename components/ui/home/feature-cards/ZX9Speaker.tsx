3ef0f5e13c5e0f09f5d406366de4a4900f1bdb35
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-21 23:53:12 +0100

    style: Increase opacity

--- components/ui/home/feature-cards/ZX9Speaker.tsx
+++ components/ui/home/feature-cards/ZX9Speaker.tsx
@@ -1,33 +1,34 @@
+
 import { zx9SpeakerText } from "@/app/lib/constants";
 import { Button } from "../../shared/Button";
 import ImageContainer from "../../shared/ImageContainer";
-import Image from "next/image";
+
 
 export const ZX9Speaker = () => {
+
  return (
-    <div className="bg-orange-500 bg-pattern-circle-svg flex">
-        <div>
+    <div className="bg-orange-secondary bg-pattern-circle-svg bg-top bg-contain lg:bg-contain lg:bg-repeat lg:bg-left bg-no-repeat flex flex-col lg:flex-row justify-center items-center my-10 rounded-lg">
+        <div className="mt-14 lg:-mb-2 overflow-hidden">
             <ImageContainer 
               mobileSrc="/home/mobile/image-speaker-zx9.png"
               tabletSrc="/home/tablet/image-speaker-zx9.png"
               desktopSrc="/home/desktop/image-speaker-zx9.png"
-              mobileHeight={200}
-              tabletHeight={300}
-              desktopHeight={400}
-              mobileWidth={200}
-              tabletWidth={300}
+              mobileHeight={100}
+              mobileWidth={160}
+              tabletWidth={200}
+              tabletHeight={100}
+              desktopHeight={500}
               desktopWidth={400}
               alt='ZX9 Speakers'
               
-            />
-            
+            />            
         </div>
-        <div>
-            <h1>
+        <div className="text-white-text flex flex-col justify-center text-center lg:ml-20 lg:text-left">
+            <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold uppercase w-2/4 mx-auto lg:mx-0 mt-7">
                 zx9 speaker
-            </h1>
-            <p>{zx9SpeakerText}</p>
-            <Button className="bg-black-900"/>
+            </h2>
+            <p className=" text-white-text/70 text-sm w-7/10 lg:mx-0 lg:w-2/4 mx-auto mt-8 mb-10">{zx9SpeakerText}</p>
+            <Button className="bg-black-900 mb-12 md:mt-5 md:mb-16"/>
         </div>
     </div>
   );
--- components/ui/shared/ImageContainer.tsx
+++ components/ui/shared/ImageContainer.tsx
@@ -2,29 +2,39 @@ import Image, { ImageProps } from "next/image";
 
 interface Props extends Omit<ImageProps, 'src'> {
     mobileSrc: string,
-    mobileWidth: number,
-    mobileHeight: number,
+    mobileWidth?: number,
+    mobileHeight?: number,
     tabletSrc: string,
-    tabletWidth: number,
-    tabletHeight: number,
+    tabletWidth?: number,
+    tabletHeight?: number,
     desktopSrc: string,
-    desktopWidth: number,
-    desktopHeight: number,
-    alt: string
-    className?: string
+    desktopWidth?: number,
+    desktopHeight?: number,
+    alt: string,
+    className?: string,
+    fill?: boolean
 }
 
 const ImageContainer = ({
-    mobileSrc, mobileWidth, mobileHeight,
-    tabletSrc, tabletWidth, tabletHeight,
-    desktopSrc, desktopWidth, desktopHeight,
-    alt, className, ...rest
-}: Props) => {
+        mobileSrc, mobileWidth, mobileHeight, tabletSrc, tabletWidth, tabletHeight,
+        desktopSrc, desktopWidth, desktopHeight, alt, className, fill, ...rest
+    }: Props) => {
     return (
         <>
-            <Image {...rest} src={mobileSrc} alt={alt} width={mobileWidth} height={mobileHeight}  className="sm:hidden"/>
-            <Image {...rest} src={tabletSrc} alt={alt} width={tabletWidth} height={tabletHeight}   className="hidden sm:block lg:hidden"/>
-            <Image {...rest} src={desktopSrc} alt={alt} width={desktopWidth} height={desktopHeight} className="hidden lg:block"/>
+            {
+                fill ?
+                    <>
+                        <Image {...rest} src={mobileSrc} alt={alt} fill className={`sm:hidden ${className} `}/>
+                        <Image {...rest} src={tabletSrc} alt={alt} fill className={`hidden sm:block lg:hidden ${className}`} />
+                        <Image {...rest} src={desktopSrc} alt={alt} fill className={`hidden lg:block ${className}`} />
+                    </>
+                    :
+                    <>
+                        <Image {...rest} src={mobileSrc} alt={alt} width={mobileWidth} height={mobileHeight} className={`sm:hidden ${className} `}/>
+                        <Image {...rest} src={tabletSrc} alt={alt} width={tabletWidth} height={tabletHeight} className={`hidden sm:block lg:hidden ${className}`} />
+                        <Image {...rest} src={desktopSrc} alt={alt} width={desktopWidth} height={desktopHeight} className={`hidden lg:block ${className}`} />
+                    </>
+            }
         </>
     );
 };
