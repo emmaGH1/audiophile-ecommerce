94332af7f9ddca3fcb62abfea04b6c673517906f
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-23 14:49:17 +0100

    feat: Complete zx7 implementation

--- components/ui/home/feature-cards/ZX7Speaker.tsx
+++ components/ui/home/feature-cards/ZX7Speaker.tsx
@@ -1,7 +1,26 @@
+import { Button } from "../../shared/Button";
+import ImageContainer from "../../shared/ImageContainer";
+import { Title } from "../../shared/Title";
 
 export const ZX7Speaker = () => {
   return (
-    <div>ZX7Speaker</div>
+    <div className="container relative flex flex-col justify-center w-full h-full mt-10 overflow-hidden py-36">
+      <ImageContainer 
+          mobileSrc="/home/mobile/image-speaker-zx7.jpg"
+          tabletSrc="/home/tablet/image-speaker-zx7.jpg"
+          desktopSrc="/home/desktop/image-speaker-zx7.jpg"
+          alt="ZX7 Speaker"
+          fill={true}
+          quality={100}
+          className="absolute object-cover sm:object-contain -z-10 rounded-xl"
+      />
+      <div className="flex flex-col ml-5 lg:ml-20">
+        <h1
+         className="text-3xl font-semibold uppercase sm:font-bold text-black-900 lg:font-semibold"
+        >ZX7 Speaker</h1>
+        <Button className="mt-6 transition-colors duration-300 bg-transparent border border-black-900 text-black-900 lg:font-semibold hover:bg-black-900 hover:text-white-text" />
+      </div>
+    </div>
   );
 };
 
--- components/ui/shared/Title.tsx
+++ components/ui/shared/Title.tsx
@@ -1,18 +1,17 @@
+import { CSSProperties } from "react";
 
 interface Props {
     text: string,
     className?: string
+    style?: CSSProperties
 }
 
-export const Title = ({ text, className }: Props) => {
+export const Title = ({ text, className, style }: Props) => {
   return (
-    <h1 className={`
-    font-extrabold text-4xl
-     md:text-5xl lg:text-6xl 
-     text-white-text pt-5 
-     sm:w-2/4 sm:mx-auto lg:mx-0 uppercase
-     ${className}`
-     }>
+    <h1 className={`font-extrabold text-4xl
+    md:text-5xl lg:text-6xl 
+    text-white-text pt-5 
+    sm:w-2/4 sm:mx-auto lg:mx-0 uppercase ${className}`} style={style}>
         {text}
     </h1>
   );
