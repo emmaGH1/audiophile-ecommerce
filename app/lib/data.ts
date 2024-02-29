7fe79ac61357d36cb1c9a6b7fba15e7bd1fedf0a
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-02-29 01:37:35 +0100

    feat: Update fetch function

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,14 +3,32 @@
 import { createClient } from "@/utils/supabase/server"
 
 import { cookies } from "next/headers";
+const cookieStore = cookies()
+const supabase = createClient(cookieStore);
 
-export async function fetchData() {
-    try {
-        const cookieStore = cookies()
-        const supabase = createClient(cookieStore);
-        const { data: products } = await supabase.from("products").select()
-        return products
-    } catch (error: unknown) {
+export async function fetchProductsByCategory(category: string) {
+      const { data, error } = await supabase
+      .from("products")
+      .select()
+      .eq('category', category)
+
+    if (error) {
       console.error('Error fetching data:', error);
+      throw new Error('Failed to fetch ')
     }
-  }
+    return data
+}
+
+export async function fetchProductById(slug: string) {
+    const { data, error } = await supabase
+      .from("products")
+      .select()
+      .eq('slug', slug)
+    
+    if (error) {
+      console.error('Error fetching data:', error);
+      throw new Error('Failed to fetch ')
+    }
+    return data
+}
+
