8bf0b33d4c92a5ce6902571416d30e20a19bcbd2
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-03-25 02:05:29 +0100

    feat: update button link

--- components/ui/home/HeroSection.tsx
+++ components/ui/home/HeroSection.tsx
@@ -12,7 +12,7 @@ export const HeroSection = () => {
             <div className=" tracking-[0.45em] text-base font-bold text-black-400/80 pt-24">new product</div>
             <Title text="xx99 mark ii headphones" />
             <Description text={heroText} />
-            <Button className="transition-colors duration-150 bg-orange-secondary hover:bg-orange-secondary/80 text-white-text" />
+            <Button linkTo='/headphones/xx99-mark-two-headphones' className="transition-colors duration-150 bg-orange-secondary hover:bg-orange-secondary/80 text-white-text" />
           </div>
        </div>
     </section>
